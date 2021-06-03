2bdb665e154231ed21b0b8abeab2a2802dfe89d2
zepolly <zepollyw@yandex.ru>
2021-06-03 21:30:58 +0300

    Добавляет функции

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function getRandomNumber (min, max) {
+  if (min < 0) {
+    throw new Error('Ошибка! Число должно быть положительным');
+  }
+  if (min > max) {
+    throw new Error('Ошибка! Измените диапазон');
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+getRandomNumber(1, 45);
+
+function checkMaxLengthString (line, maxLine) {
+  return (line.length <= maxLine);
+}
+checkMaxLengthString('комментарий', 30);
